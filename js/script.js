163959bde23fcd34e22c666a56f0c384f6bbeb0f
JennyFletch <111204164+JennyFletch@users.noreply.github.com>
2022-11-04 15:33:20 -0600

    Drink recipe API added and tested

--- js/script.js
+++ js/script.js
@@ -1,7 +1,11 @@
 var recipeAPIkey = "f598cd619e64455eb06d890b5d2e92ec";
-var requestURL = `https://api.spoonacular.com/recipes/findByIngredients?apiKey=${recipeAPIkey}&ingredients=apples,+flour,+sugar&number=2`;
+var foodRequestURL = `https://api.spoonacular.com/recipes/findByIngredients?apiKey=${recipeAPIkey}&ingredients=apples,+flour,+sugar&number=2`;
 
-fetch(requestURL)
+var drinkAPIkey = "1"; // free-version
+var drinkRequestURL = `https://www.thecocktaildb.com/api/json/v1/${drinkAPIkey}/filter.php?i=coffee`;
+// note: ingredients can be liquors or other items (tested lemon and coffee) but for some reason breaks on the word tonic ?? 
+
+fetch(drinkRequestURL)
 .then(function (response) {
 
      if(response.status === 200) {
